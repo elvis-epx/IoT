62195209589293d78a3ce672c43fea19648eb358
Elvis Pfutzenreuter <epxx@epxx.co>
2021-12-20 21:56:27 -0300

    Test wrap-around of millis()

---
+++ test/unit/012-wrap.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+. unit/defs.sh
+
+sensors 20 40 60 80 100
+runme
+cond state eq "Off"
+
+echo "Advancing 1M seconds"
+advance 1000000
+sleep 10
+echo "Advancing 1M seconds"
+advance 1000000
+sleep 10
+echo "Advancing 1M seconds"
+advance 1000000
+sleep 10
+echo "Advancing 1M seconds"
+advance 1000000
+sleep 10
+# Provoke wrap-around of millis()
+echo "Advancing 500k seconds"
+advance 500000
+# Check the uptime is >46 days
+sleep 10
