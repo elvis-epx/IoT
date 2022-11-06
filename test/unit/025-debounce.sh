fe848c6058823a1b1475f4c49b2130afe0ce6d2c
Elvis Pfutzenreuter <epxx@epxx.co>
2022-11-06 12:35:43 -0300

    Improve debouncing tests

--- test/unit/025-debounce.sh
+++ test/unit/025-debounce.sh
@@ -7,7 +7,16 @@ runme
 sleep 10
 cond level% eq 60
 sensors 20 40 60 80
-sleep 2
+sleep 1
 cond level% eq 60
-sleep 5
+sleep 1
+cond level% eq 60
+sleep 1
+cond level% eq 60
+sleep 2
 cond level% eq 80
+sensors 20 40 60 80 100
+sleep 2
+cond level% eq 80
+sleep 3
+cond level% eq 100
