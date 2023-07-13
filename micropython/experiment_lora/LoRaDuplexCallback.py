61e946a6b78a5aa5f58b5d4bca5b30930f505ced
Elvis Pfützenreuter <epxx@epxx.co>
2023-07-13 16:56:42 -0300

    Add Reed-Solomon

--- micropython/experiment_lora/LoRaDuplexCallback.py
+++ micropython/experiment_lora/LoRaDuplexCallback.py
@@ -1,11 +1,17 @@
 from time import ticks_ms
 import random
+from reedsolo import RSCodec
+import gc
+
+rs = RSCodec(6)
 
 msgCount = 0            # count of outgoing messages
 INTERVAL = 5000         # interval between sends
 INTERVAL_BASE = 5000    # interval between sends base
 
 display = None
+
+received = []
 
 def duplexCallback(lora, pdisplay):
     global display
@@ -22,13 +28,33 @@ def do_loop(lora):
     interval = 0
 
     while True:
+        if received:
+            payload, rssi = received[0]
+            received.pop(0)
+            print("*** Received message len {} RSSI {}".format(len(payload), rssi))
+            try:
+                msg, _, errors = rs.decode(payload)
+                errors = len(errors)
+            except:
+                errors = 999
+
+            display.fill(0)
+            display.text("Recv RSSI {}".format(rssi), 0, 0, 1)
+            display.text("size {}".format(len(payload)), 0, 12, 1)
+            display.text("errors {}".format(errors), 0, 24, 1)
+            display.show()
+
+            gc.collect()
+            continue
+
         now = ticks_ms()
 
         if (now - lastSendTime > interval):
             lastSendTime = now                                      # timestamp the message
             interval = int(INTERVAL_BASE + random.random() * INTERVAL)
 
-            message = "abracadabra %06d" % msgCount
+            message = ("abracadabra %06d" % msgCount).encode()
+            message = rs.encode(message)
             t0 = ticks_ms()
             sendMessage(lora, message)                              # send message
             t1 = ticks_ms()
@@ -44,13 +70,9 @@ def do_loop(lora):
 
 
 def sendMessage(lora, outgoing):
-    lora.send(outgoing.encode())
+    lora.send(outgoing)
 
 
 def on_receive(lora, payload):
     rssi = lora.packetRssi()
-    print("*** Received message len {} RSSI {}".format(len(payload), rssi))
-    display.fill(0)
-    display.text("Recv RSSI {}".format(rssi), 0, 0, 1)
-    display.text("size {}".format(len(payload)), 0, 12, 1)
-    display.show()
+    received.append((payload, rssi))
