3ed1e203689501b5e0941191856c0380b47fa539
Elvis Pfutzenreuter <epxx@epxx.co>
2023-09-11 16:50:01 -0300

    Adicionar driver

--- micropython/lib/switch/driver.py
+++ micropython/lib/switch/driver.py
@@ -44,3 +44,25 @@ class dtwonder2:
         for i, pin in enumerate(self.input_pins):
             bitmap += (pin.value() and 1 or 0) << i
         return ~bitmap
+
+# PLC HF-006 Hofffer Automação https://hofferautomacao.com/
+
+class hoffer6:
+    def __init__(self):
+        self.inputs = 8
+        self.outputs = 6
+        self.led = 2
+
+        self.input_pins_no = [36, 39, 32, 33, 25, 26, 27, 14]
+        self.output_pins_no = [4, 16, 17, 18, 19, 23]
+        self.input_pins = [ Pin(n, Pin.IN) for n in self.input_pins_no ]
+        self.output_pins = [ Pin(n, Pin.OUT) for n in self.output_pins_no ]
+
+    def output_pin(self, pin, value):
+        self.output_pins[pin].value(value)
+
+    def input(self):
+        bitmap = 0
+        for i, pin in enumerate(self.input_pins):
+            bitmap += (pin.value() and 1 or 0) << i
+        return bitmap
