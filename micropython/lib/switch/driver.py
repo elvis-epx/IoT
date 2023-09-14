88806dfa17751c550c4dbf724cc710a3fdce8222
Elvis Pfützenreuter <epxx@epxx.co>
2023-09-13 21:06:23 -0300

    Add DTWonder 4-port

--- micropython/lib/switch/driver.py
+++ micropython/lib/switch/driver.py
@@ -45,6 +45,26 @@ class dtwonder2:
             bitmap += (pin.value() and 1 or 0) << i
         return ~bitmap
 
+class dtwonder4:
+    def __init__(self):
+        self.inputs = 4
+        self.outputs = 4
+        self.led = -1 # none
+
+        self.input_pins_no = [36, 39, 33, 35]
+        self.output_pins_no = [16, 2, 32, 12]
+        self.input_pins = [ Pin(n, Pin.IN) for n in self.input_pins_no ]
+        self.output_pins = [ Pin(n, Pin.OUT) for n in self.output_pins_no ]
+
+    def output_pin(self, pin, value):
+        self.output_pins[pin].value(value)
+
+    def input(self):
+        bitmap = 0
+        for i, pin in enumerate(self.input_pins):
+            bitmap += (pin.value() and 1 or 0) << i
+        return ~bitmap
+
 # PLC HF-006 Hofffer Automação https://hofferautomacao.com/
 
 class hoffer6:
