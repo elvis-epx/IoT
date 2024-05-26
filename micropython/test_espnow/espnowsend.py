3085ffc72a6f77aa2013d532686fa20bf61f1480
Elvis Pfutzenreuter <epxx@epxx.co>
2024-05-26 14:43:56 -0300

    Changes in espnow test

--- micropython/test_espnow/espnowsend.py
+++ micropython/test_espnow/espnowsend.py
@@ -7,12 +7,14 @@ sta.config(channel=6)
 e = espnow.ESPNow()
 e.active(True)
 
-peer = b'\xff' * 6
+peer = b'\xc8\xf0\x9eM\t\x0c'
+# peer = b'\xff' * 6
 e.add_peer(peer)
 counter = 0
 
 while True:
     counter += 1
+    counter %= 100
     e.send(peer, b'ping %d' % counter)
     print("Sent")
-    time.sleep(1)
+    time.sleep(2)
