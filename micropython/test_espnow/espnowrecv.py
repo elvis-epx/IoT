4f7cbee510027cdbfe18906abc5dfd37a1a984f6
Elvis Pfutzenreuter <epxx@epxx.co>
2024-06-01 14:15:39 -0300

    Changes to test PMK and LMK

--- micropython/test_espnow/espnowrecv.py
+++ micropython/test_espnow/espnowrecv.py
@@ -1,13 +1,19 @@
 import network, espnow, time
 
 sta = network.WLAN(network.STA_IF); sta.active(False)
-sta.active(True)
 sta.config(channel=6)
+sta.active(True)
 
 e = espnow.ESPNow()
 e.active(True)
+e.set_pmk("foobar7890123456")
+peer = bytes(int(x, 16) for x in "c8:f0:9e:4d:09:0c".split(":"))
+e.add_peer(peer, "barfoo7890123456")
 
 while True:
     if e.any():
         print(e.recv())
         print(e.peers_table)
+    else:
+        print("No data")
+    time.sleep(2)
--- micropython/test_espnow/espnowsend.py
+++ micropython/test_espnow/espnowsend.py
@@ -2,19 +2,21 @@ import network, espnow, time
 
 sta = network.WLAN(network.STA_IF); sta.active(False)
 sta.active(True)
-sta.config(channel=6)
+sta.config(channel=1)
 
 e = espnow.ESPNow()
 e.active(True)
+e.set_pmk("foobar7890123456") # must be after active(True)
 
-peer = b'\xc8\xf0\x9eM\t\x0c'
-# peer = b'\xff' * 6
-e.add_peer(peer)
+peer = bytes(int(x, 16) for x in "c4:dd:57:ea:90:e0".split(":"))
+e.add_peer(peer, "barfoo7890123456")
 counter = 0
 
 while True:
     counter += 1
     counter %= 100
-    e.send(peer, b'ping %d' % counter)
-    print("Sent")
-    time.sleep(2)
+    if e.send(peer, b'ping %d' % counter):
+        print("Sent")
+    else:
+        print("Not sent")
+    time.sleep(3)
