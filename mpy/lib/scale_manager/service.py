484c2f82f5296db0b37ef45d12155edd5b35492c
Elvis Pfutzenreuter <epxx@epxx.co>
2024-07-07 16:27:27 -0300

    Revert "Autopair"
    This reverts commit af8f5adcacef2f4952c51b810e86b2d2556b8e69.

--- mpy/lib/scale_manager/service.py
+++ mpy/lib/scale_manager/service.py
@@ -1,4 +1,4 @@
-from epx.mqtt import MQTTPub
+from epx.mqtt import MQTTPub, MQTTSub
 from epx.loop import SECONDS, MINUTES
 
 class WeightPub(MQTTPub):
@@ -20,3 +20,12 @@ class MalfunctionPub(MQTTPub):
     def gen_msg(self):
         w = self.sensor.malfunction()
         return (w is not None) and ("%d" % w) or None
+
+class PairSub(MQTTSub):
+    def __init__(self, netnow):
+        MQTTSub.__init__(self, "cmnd/%s/OpenToPair")
+        self.netnow = netnow
+
+    def recv(self, topic, msg, retained, dup):
+        if len(msg) > 0:
+            self.netnow.opentopair()
--- mpy/scale_manager_main.py
+++ mpy/scale_manager_main.py
@@ -8,6 +8,7 @@ from epx.net import Net
 from epx.netnow import NetNowCentral
 from epx.mqtt import MQTT, MQTTSub
 from scale_manager.forwarder import Forwarder
+from scale_manager.service import PairSub
 
 config = Config()
 nvram = NVRAM("scale_manager")
@@ -16,5 +17,7 @@ net = Net(config)
 netnow = NetNowCentral(config, nvram, net)
 mqtt = MQTT(config, net, watchdog)
 forwarder = Forwarder(config, netnow, mqtt)
+pair = PairSub(netnow)
+mqtt.sub(pair)
 
 loop.run()
