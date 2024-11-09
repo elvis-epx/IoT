2ca9b4b0606b1f9ad95e90188d269404826c7a25
Elvis Pfutzenreuter <epxx@epxx.co>
2024-11-09 18:33:18 -0300

    433 experiment

--- experiments/433/main.py
+++ experiments/433/main.py
@@ -3,44 +3,55 @@ from time import ticks_us, ticks_add, ticks_diff
 from micropython import schedule
 
 last_timestamp = 0
-last_value = 0
-in_seq = False
+irq_count = 0
 
-sequence = []
+TRANS_MAX = 200
+trans_sequence = [ [0, 0] for _ in range(0, TRANS_MAX) ]
+trans_length = 0
+closed_sequence = [ [0, 0] for _ in range(0, TRANS_MAX) ]
+closed_length = 0
+closed_sequence_free = True
 
-def data_transition(pair):
-    sequence.append(pair)
-
-def close_sequence(_):
-    global sequence
-    seq = sequence
-    sequence = []
-    print(seq)
+def parse_sequence(_):
+    global closed_sequence_free
+    print(closed_sequence[0:closed_length])
+    closed_sequence_free = True
 
 def irq(p):
-    global last_timestamp, last_value, in_seq
+    global last_timestamp, irq_count
+    global trans_sequence, trans_length
+    global closed_sequence, closed_length, closed_sequence_free
 
     # if value is 1, it means it has been 0
-    v = p.value()
-    if v == last_value:
-        return
-    last_value = v
+    v = not not p.value()
 
     t = ticks_us()
     dt = ticks_diff(t, last_timestamp)
     last_timestamp = t
 
-    if dt < 150 or dt > 50000:
+    irq_count += 1
+
+    if dt < 150 or dt > 50000 or trans_length >= (TRANS_MAX - 1):
         # bad transition
-        if in_seq:
-            schedule(close_sequence, None)
-            in_seq = False
+        if trans_length == 0:
+            return
+
+        # TODO handle long transitions as sequence terminators
+
+        if trans_length > 10 and closed_sequence_free:
+            # swap buffers
+            closed_sequence, trans_sequence = trans_sequence, closed_sequence
+            closed_length = trans_length
+            closed_sequence_free = False
+            schedule(parse_sequence, None)
+
+        trans_length = 0
         return
 
-    # good transition
-    # send !v instead of v since e.g. if it is 1, it has been 0 for time dt
-    in_seq = True
-    schedule(data_transition, (not not v, dt))
+    # good transition; annotate
+    trans_sequence[trans_length][0] = v
+    trans_sequence[trans_length][1] = dt
+    trans_length += 1
 
 pin = Pin(14, Pin.IN)
 pin.irq(trigger = Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=irq)
