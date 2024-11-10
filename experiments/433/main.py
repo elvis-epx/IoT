e3de183b84840276046d9fd90b770827e50180c0
Elvis Pfützenreuter <epxx@epxx.co>
2024-11-09 22:41:39 -0300

    Fix bug, improvement

--- experiments/433/main.py
+++ experiments/433/main.py
@@ -2,36 +2,47 @@ from machine import Pin
 from time import ticks_us, ticks_add, ticks_diff
 from micropython import schedule
 
-IDLE = 0
-DATA = 1
-FULL = 2
+IDLE = const(0)
+DATA = const(1)
+FULL = const(2)
 
 last_timestamp = 0
 irq_count = 0
 state = IDLE
 
-TRANS_MAX = 100
-RING_BUF = 10
+TRANS_MAX = const(100)
+RING_BUF = const(10)
 trans_sequence = [ [ [0, 0] for _ in range(0, TRANS_MAX) ] for _ in range(0, RING_BUF) ]
 trans_length = [ 0 for _ in range(0, RING_BUF) ]
 i = 0
-ready = 0
+to_parse = 0
+
+# Typical preamble length is 10k-12kµs
+PREAMBLE_MIN = const(6000)
+PREAMBLE_MAX = const(20000)
+# EV1527 = 230µs, HT6P20 = 500µs
+DATA_MIN = const(120)
+# EV1527 = 3x min, HT6P20 = 2x min
+DATA_MAX = const(1800)
+# 24 bits = 48 transitions for EV1527, 28 bits for HT6P20
+TRANS_COUNT_MIN = const(30)
 
 def parse_sequence(_):
-    global trans_sequence, trans_length, i, ready
-    while ready > 0:
-        j = (i - ready) % RING_BUF
+    global trans_sequence, trans_length, i, to_parse
+    while to_parse > 0:
+        j = (i - to_parse) % RING_BUF
         print(trans_sequence[j][0:trans_length[j]])
-        ready -= 1
+        to_parse -= 1
 
 def irq(p):
-    global last_timestamp, irq_count
+    global last_timestamp, irq_count, state
     global trans_sequence, trans_length
-    global i, ready, state
+    global i, to_parse
 
     # if value is 1, it means it has been 0
-    v = not not p.value()
+    v = (p.value() + 1) % 2
 
+    # Calculate pulse length
     t = ticks_us()
     dt = ticks_diff(t, last_timestamp)
     last_timestamp = t
@@ -39,19 +50,19 @@ def irq(p):
     irq_count += 1
 
     if state == FULL:
-        if ready >= RING_BUF:
+        if to_parse >= RING_BUF:
             return
         state = IDLE
 
     if state == IDLE:
-        if dt > 6000 and dt < 100000 and not v:
+        if dt > PREAMBLE_MIN and dt < PREAMBLE_MAX and v == 0:
             # detected preamble
             state = DATA
             trans_length[i] = 0
         return
 
     if state == DATA:
-        if dt > 150 and dt < 1500:
+        if dt > DATA_MIN and dt < DATA_MAX:
             # chirps of data
             trans_sequence[i][trans_length[i]][0] = v
             trans_sequence[i][trans_length[i]][1] = dt
@@ -62,20 +73,20 @@ def irq(p):
             return
 
         # Sequence terminated by silence or bad transition
-        if trans_length[i] > 20:
+        state = IDLE
+
+        if trans_length[i] > TRANS_COUNT_MIN:
             # Export sequence
-            ready += 1
+            to_parse += 1
             i = (i + 1) % RING_BUF
             schedule(parse_sequence, None)
 
-            if ready >= RING_BUF:
+            if to_parse >= RING_BUF:
                 # Must not overwrite the buffer pointed by "i" for now
                 state = FULL
                 return
 
-        state = IDLE
-
-        if dt > 6000 and dt < 100000 and not v:
+        if dt > PREAMBLE_MIN and dt < PREAMBLE_MAX and v == 0:
             # new preamble (back-to-back packet)
             # short-circuit state machine to DATA
             trans_length[i] = 0
