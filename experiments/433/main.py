bb7806e03019cf425587947770822a21433bd7a5
Elvis Pfutzenreuter <epxx@epxx.co>
2024-11-09 22:02:10 -0300

    Improvements

--- experiments/433/main.py
+++ experiments/433/main.py
@@ -2,25 +2,32 @@ from machine import Pin
 from time import ticks_us, ticks_add, ticks_diff
 from micropython import schedule
 
+IDLE = 0
+DATA = 1
+FULL = 2
+
 last_timestamp = 0
 irq_count = 0
+state = IDLE
 
-TRANS_MAX = 200
-trans_sequence = [ [0, 0] for _ in range(0, TRANS_MAX) ]
-trans_length = 0
-closed_sequence = [ [0, 0] for _ in range(0, TRANS_MAX) ]
-closed_length = 0
-closed_sequence_free = True
+TRANS_MAX = 100
+RING_BUF = 10
+trans_sequence = [ [ [0, 0] for _ in range(0, TRANS_MAX) ] for _ in range(0, RING_BUF) ]
+trans_length = [ 0 for _ in range(0, RING_BUF) ]
+i = 0
+ready = 0
 
 def parse_sequence(_):
-    global closed_sequence_free
-    print(closed_sequence[0:closed_length])
-    closed_sequence_free = True
+    global trans_sequence, trans_length, i, ready
+    while ready > 0:
+        j = (i - ready) % RING_BUF
+        print(trans_sequence[j][0:trans_length[j]])
+        ready -= 1
 
 def irq(p):
     global last_timestamp, irq_count
     global trans_sequence, trans_length
-    global closed_sequence, closed_length, closed_sequence_free
+    global i, ready, state
 
     # if value is 1, it means it has been 0
     v = not not p.value()
@@ -31,27 +38,48 @@ def irq(p):
 
     irq_count += 1
 
-    if dt < 150 or dt > 50000 or trans_length >= (TRANS_MAX - 1):
-        # bad transition
-        if trans_length == 0:
+    if state == FULL:
+        if ready >= RING_BUF:
             return
+        state = IDLE
 
-        # TODO handle long transitions as sequence terminators
-
-        if trans_length > 10 and closed_sequence_free:
-            # swap buffers
-            closed_sequence, trans_sequence = trans_sequence, closed_sequence
-            closed_length = trans_length
-            closed_sequence_free = False
-            schedule(parse_sequence, None)
-
-        trans_length = 0
+    if state == IDLE:
+        if dt > 6000 and dt < 100000 and not v:
+            # detected preamble
+            state = DATA
+            trans_length[i] = 0
         return
 
-    # good transition; annotate
-    trans_sequence[trans_length][0] = v
-    trans_sequence[trans_length][1] = dt
-    trans_length += 1
+    if state == DATA:
+        if dt > 150 and dt < 1500:
+            # chirps of data
+            trans_sequence[i][trans_length[i]][0] = v
+            trans_sequence[i][trans_length[i]][1] = dt
+            trans_length[i] += 1
+            if trans_length[i] >= TRANS_MAX:
+                # overflow, discard
+                state = IDLE
+            return
+
+        # Sequence terminated by silence or bad transition
+        if trans_length[i] > 20:
+            # Export sequence
+            ready += 1
+            i = (i + 1) % RING_BUF
+            schedule(parse_sequence, None)
+
+            if ready >= RING_BUF:
+                # Must not overwrite the buffer pointed by "i" for now
+                state = FULL
+                return
+
+        state = IDLE
+
+        if dt > 6000 and dt < 100000 and not v:
+            # new preamble (back-to-back packet)
+            # short-circuit state machine to DATA
+            trans_length[i] = 0
+            state = DATA
 
 pin = Pin(14, Pin.IN)
 pin.irq(trigger = Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=irq)
